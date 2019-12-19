2aced45ee09bbd211c92940b6f42a807f04adff1
Hande Zhou <45753570+Hande-hub@users.noreply.github.com>
2019-12-19 01:33:44 -0600

    Spring Boot Service Unit (#2)
    Resolve #1
    
    Signed-off-by: Hande Zhou <hande.zhou@foxmail.com>

---
+++ src/main/java/com/example/springbootunitdemo/Serviceut.java
@@ -0,0 +1,11 @@
+package com.example.springbootunitdemo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Serviceut {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
